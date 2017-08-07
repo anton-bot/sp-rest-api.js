35c9a69bacf8ee27d06a09ad271814d8e51ce9cb
Anton Ivanov <anton@imago.ai>
2017-08-07 09:30:51 +0800

    restructure as a class

--- sp-rest-api.js
+++ sp-rest-api.js
@@ -9,7 +9,7 @@
  * @typedef {Object} SpRestApi
  * Contains methods for calling the SharePoint REST API.
  */
-var SpRestApi = {
+var SpRestApi = function () {
     /**
      * @typedef {Object} SpRestApiOptions - The options passed to methods
      *      inside the SpRestApi class.
@@ -27,59 +27,58 @@ var SpRestApi = {
      *      This is to overcome SharePoint's limitation of maximum 5000 items
      *      per call.
      */
+};
 
+/**
+ * Sets the list title (list display name) of this SpRestApi instance.
+ * The .lists() must be called before any call to other list-related
+ * methods.
+ * @param {string} listTitle - The display name of the SharePoint list.
+ * @returns {SpRestApi} Returns the instance of this SpRestApi object.
+ */
+SpRestApi.prototype.lists = function (listTitle) {
+    this.listTitle = listTitle;
+    return this;
+}
 
-    /**
-     * Sets the list title (list display name) of this SpRestApi instance.
-     * The .lists() must be called before any call to other list-related
-     * methods.
-     * @param {string} listTitle - The display name of the SharePoint list.
-     * @returns {SpRestApi} Returns the instance of this SpRestApi object.
-     */
-    lists: function (listTitle) {
-        this.listTitle = listTitle;
-        return this;
-    },
+/**
+ * Sets the SpRestApiOptions. If not called, before the request to server,
+ * the default options will be used.
+ * @param {SpRestApiOptions} options
+ */
+SpRestApi.prototype.options = function (options) {
+    // TODO FIXME merge with default options
+    this.options = options;
+};
 
-    /**
-     * Sets the SpRestApiOptions. If not called, before the request to server,
-     * the default options will be used.
-     * @param {SpRestApiOptions} options
-     */
-    options: function (options) {
-        // TODO FIXME merge with default options
-        this.options = options;
-    },
+/**
+ * Returns all items from a list, or all items up to the SharePoint limit
+ * or the limit specified in the options.
+ */
+SpRestApi.prototype.getAllItems = function () {
 
-    /**
-     * Returns all items from a list, or all items up to the SharePoint limit
-     * or the limit specified in the options.
-     */
-    getAllItems: function () {
-        
-    },
+};
 
-    /**
-     * A generic function to call any URL of the SharePoint REST API. Usually
-     * there is no need to call this method directly. 
-     */
-    loadUrl: function (url, method, success, error) {
-        $.ajax({
-            url: url,
-            type: method,
-            cache: false,
-            contentType: "application/json; odata=verbose",
-            headers: {
-                "Accept": "application/json; odata=verbose",
-                "X-RequestDigest":
-                    document.getElementById("__REQUESTDIGEST").value // TODO FIXME get via a separate function or property
-            },
-            success: function (data) {
-                success(data);
-            },
-            error: function (data) {
-                error(data);
-            },
-        });
-    }
+/**
+ * A generic function to call any URL of the SharePoint REST API. Usually
+ * there is no need to call this method directly. 
+ */
+SpRestApi.prototype.loadUrl: function (url, method, success, error) {
+    $.ajax({
+        url: url,
+        type: method,
+        cache: false,
+        contentType: "application/json; odata=verbose",
+        headers: {
+            "Accept": "application/json; odata=verbose",
+            "X-RequestDigest":
+                document.getElementById("__REQUESTDIGEST").value // TODO FIXME get via a separate function or property
+        },
+        success: function (data) {
+            success(data);
+        },
+        error: function (data) {
+            error(data);
+        },
+    });
 };
