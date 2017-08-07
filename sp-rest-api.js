d2cd95ff252f6ac4eb9931414c48589cca8a57bb
Anton Ivanov <anton@imago.ai>
2017-08-07 09:42:33 +0800

    add default options and verbosity enum

--- sp-rest-api.js
+++ sp-rest-api.js
@@ -10,23 +10,54 @@
  * Contains methods for calling the SharePoint REST API.
  */
 var SpRestApi = function () {
-    /**
-     * @typedef {Object} SpRestApiOptions - The options passed to methods
-     *      inside the SpRestApi class.
-     * @property {Function} onsuccess - The callback function for successful
-     *      requests to SharePoint REST API.
-     * @property {Function} onerror - The callback function for failed requests
-     *      to SharePoint REST API.
-     * @property {number} maxItems - The maximum number of items to be returned
-     *      from the list. If `recursiveFetch` is set to true, this is the
-     *      maximum number of items to fetch on each request to server. If not
-     *      not specified, defaults to SharePoint's limit of 100. Maximum is
-     *      5000 due to SharePoint limitations.
-     * @property {boolean} recursiveFetch - Fetch all items from the list by
-     *      repeatedly making server requests until all list items are fetched.
-     *      This is to overcome SharePoint's limitation of maximum 5000 items
-     *      per call.
-     */
+
+};
+
+/**
+ * @typedef {Object} SpRestApiOptions - The options passed to methods
+ *      inside the SpRestApi class.
+ * @property {Function} onsuccess - The callback function for successful
+ *      requests to SharePoint REST API.
+ * @property {Function} onerror - The callback function for failed requests
+ *      to SharePoint REST API.
+ * @property {number} maxItems - The maximum number of items to be returned
+ *      from the list. If `recursiveFetch` is set to true, this is the
+ *      maximum number of items to fetch on each request to server. If not
+ *      not specified, defaults to SharePoint's limit of 100. Maximum is
+ *      5000 due to SharePoint limitations.
+ * @property {boolean} recursiveFetch - Fetch all items from the list by
+ *      repeatedly making server requests until all list items are fetched.
+ *      This is to overcome SharePoint's limitation of maximum 5000 items
+ *      per call.
+ * @property {string} verbosity - The amount of metadata to be returned
+ *      in the JSON response from server. Use the SpRestApi.Verbosity enum.
+ */
+
+/**
+ * The default options that will be used unless overridden.
+ * @type {SpRestApiOptions}
+ */
+SpRestApi.prototype.defaultOptions = {
+    onsuccess: console.log,
+    onerror: console.log,
+    maxItems: 5000, 
+    recursiveFetch: true,
+    verbosity: SpRestApi.Verbosity.COMPACT,
+};
+
+/**
+ * Determines the amount of metadata in the response JSON from server.
+ * For most cases, 'COMPACT' should be enough.
+ * @readonly
+ * @enum {string}
+ * @typedef Verbosity
+ */
+SpRestApi.prototype.Verbosity = {
+    /** Values are placed in `.value`. Less metadata. */
+    COMPACT: 'application/json; odata=nometadata',
+
+    /** Values are placed in `.d`. More metadata. */
+    VERBOSE: 'application/json; odata=verbose',
 };
 
 /**
